a81be55b5819dd0c29d5ca503d4368ef72f36852
dcronkite <dcronkite@gmail.com>
2022-05-13 13:20:29 -0700

    Added logging and documentation

--- src/mml_utils/scripts/extract_data_for_review.py
+++ src/mml_utils/scripts/extract_data_for_review.py
@@ -1,3 +1,55 @@
+"""
+Extract CUIs and text strings for some feature (e.g., fever, diarrhea, etc.) from MML output.
+These can then be used to review the results to determine how useful the CUIs/text strings are.
+
+Runs on Metamaplite output directories (text file and jsonl/mmi expected in same directory).
+
+Expects a feature directory:
+* {feature_name}.string.txt
+    * unique text string to look for on each line
+    * Examples:
+        * BM frequent
+        * BM loose
+* {feature_name}.cui.txt
+    * unique CUI to look for on each line
+    * additional data can be included in tab-separated format
+    * Examples:
+        * C0687681
+        * C0687681\tFeeling feverish
+
+
+# Generate Reviews for Output Data
+
+```python
+import pandas as pd
+from loguru import logger
+
+SAMPLE_SIZE = 50
+df = pd.GET_METADATA()  # e.g., with studyid and docid
+with pd.ExcelWriter(outpath / f'features.review{SAMPLE_SIZE}.xlsx') as writer:
+    logger.info(f"All notes dataset -> Unique Notes: {df['note_id'].nunique()}; Unique Patients: {df['studyid'].nunique()}")
+    for file in base_path.glob('*.review.csv'):
+        feature_name = file.stem.split('.')[0]
+        logger.info(f'Reading feature {feature_name}: {file}')
+        feature_df = pd.read_csv(file)
+        logger.info(f"Source feature dataset -> Unique Notes: {feature_df['docid'].nunique()}")
+        mdf = pd.merge(df, feature_df, on='docid')
+        logger.info(f"Merged dataset -> Unique Notes: {mdf['docid'].nunique()}; Unique Patients: {mdf['studyid'].nunique()}")
+        # output options
+        mdf['Is this term referring to the feature?'] = ''
+        mdf['Does the patient have this feature?'] = ''
+        mdf['Optional Comments'] = ''
+        unique_notes = mdf['docid'].unique()
+        sampled = random.sample(sorted(unique_notes), SAMPLE_SIZE)
+        rdf = mdf[mdf.docid.isin(sampled)]
+        rdf = rdf[[
+            'id', 'studyid', 'docid', 'precontext', 'keyword', 'postcontext', 'Is this term referring to the feature?',
+            'Does the patient have this feature?', 'Optional Comments', 'fullcontext'
+        ]]
+        rdf.to_csv(outpath / f'{feature_name}.sample{SAMPLE_SIZE}.csv', index=False)
+        rdf.to_excel(writer, sheet_name=feature_name, index=False)
+```
+"""
 import csv
 import pathlib
 import re
@@ -101,6 +153,7 @@ def extract_data_for_review(note_directories: list[pathlib.Path], target_path: p
     """
     for feature_name in get_feature_names_from_directory(target_path):
         target_cuis = load_first_column(target_path / f'{feature_name}.cui.txt')
+        logger.info(f'Starting "{feature_name}" with {len(target_cuis)} target CUIs.')
         # create pattern: sort by longest to ensure longest regex is matched first
         target_pattern = re.sub(
             r'\s+', r'\\s+',
@@ -117,12 +170,17 @@ def extract_data_for_review(note_directories: list[pathlib.Path], target_path: p
             )
             writer.writeheader()
             for note_directory in note_directories:
+                logger.info(f'Reading directory {note_directory}.')
+                note_count = 0
+                no_text_file_count = 0
                 for mml_file in note_directory.glob(f'*.{mml_format}'):
                     txt_file = note_directory / f'{mml_file.stem}{text_extension}'
                     if not txt_file.exists():
                         logger.warning(f'Failed to find corresponding text file:'
                                        f' {txt_file.name} (extension: {text_extension}).')
+                        no_text_file_count += 1
                         continue
+                    note_count += 1
                     with open(txt_file, encoding=text_encoding, errors=text_errors) as fh:
                         text = fh.read()
                     if add_cr:
@@ -174,6 +232,9 @@ def extract_data_for_review(note_directories: list[pathlib.Path], target_path: p
                             'fullcontext': clean_text(text[max(0, start - 250): end + 250]),
                         })
                         unique_id += 1
+                logger.info(f'Completed processing {note_count} notes (Total Matches: {unique_id}).')
+                if no_text_file_count:
+                    logger.warning(f'Failed to find {no_text_file_count} text files.')
 
 
 def load_first_column(file: pathlib.Path):
