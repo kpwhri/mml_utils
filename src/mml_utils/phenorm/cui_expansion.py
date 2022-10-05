826e35c2657f59a8a0e830a4d0903c4dd671ee1d
dcronkite <dcronkite@gmail.com>
2022-10-04 20:25:50 -0700

    Refactored running afep algorithm to make the code more readable

---
+++ src/mml_utils/phenorm/cui_expansion.py
@@ -0,0 +1,46 @@
+import re
+from loguru import logger
+
+try:
+    from umls_api_tool.auth import FriendlyAuthenticator
+
+    UMLS_API_TOOL_INSTALLED = True
+except ImportError:
+    logger.warning(f'UMLS API Tool not installed. CUI expansion not available.')
+    UMLS_API_TOOL_INSTALLED = False
+
+
+def add_shorter_match_cuis(results, apikey):
+    if UMLS_API_TOOL_INSTALLED:
+        logger.info(f'Trying to expand CUIs...')
+        auth = FriendlyAuthenticator.from_apikey(apikey)
+        known_cuis = {}  # cui -> new terms
+        new_results = []
+        new_cui_count = 0
+        for row in results:
+            target_cui = row['cui']
+            if target_cui not in known_cuis:
+                target_name = row['preferredname']
+                curr_results = []
+                if re.search(r'\b(?:or|and)\b', target_name, re.I):
+                    for term in re.split(r'\W+', target_name):
+                        if term.lower() in {'and', 'or', ''}:
+                            continue
+                        for cui_data in auth.search(term, language='ENG', limit_pages=1):
+                            if cui_data['name'].lower() == term.lower():
+                                curr_results.append({
+                                    'cui': cui_data['cui'],
+                                    'matchedtext': cui_data['name'],
+                                    'length': len(cui_data['name']),
+                                    'offset': target_name.index(term)
+                                })
+                                new_cui_count += 1
+                                break  # go to next word
+                known_cuis[target_cui] = curr_results
+            for res in known_cuis[target_cui]:
+                # update cui, matchedtext, start, and length
+                new_results.append(row | res | {'start': row['start'] + res['offset']})
+        logger.info(f'Added {new_cui_count} new CUIs from CUI expansion.')
+        logger.info(f'{len(new_results)} new results added by CUI expansion.')
+        results += new_results
+    return results
--- src/mml_utils/scripts/run_afep.py
+++ src/mml_utils/scripts/run_afep.py
@@ -7,24 +7,15 @@ Implementation notes:
     * Article type is defined as all content before the first '_' (not including extension)..
 """
 import datetime
-import json
 import math
 import pathlib
-import re
 
 import click
 import pandas as pd
 from loguru import logger
 
-from mml_utils.parse.mmi import extract_mml_from_mmi_data
-from mml_utils.parse.json import extract_mml_from_json_data
-
-try:
-    from umls_api_tool.auth import FriendlyAuthenticator
-    UMLS_API_TOOL_INSTALLED = True
-except ImportError:
-    logger.warning(f'UMLS API Tool not installed. CUI expansion not available.')
-    UMLS_API_TOOL_INSTALLED = False
+from mml_utils.phenorm.afep import extract_articles, run_greedy_algorithm
+from mml_utils.phenorm.cui_expansion import add_shorter_match_cuis
 
 
 @click.command()
@@ -37,8 +28,10 @@ except ImportError:
               help='For CUIs with and/or, look for subterms. This helps to comabat longest match in, e.g., MML.')
 @click.option('--apikey', default=None, type=str,
               help='API key for use when trying to expand CUIs.')
+@click.option('--skip-greedy-algorithm', is_flag=True, default=False,
+              help='Skip greedy selection algorithm.')
 def run_afep_algorithm(note_directories, *, mml_format='json', outdir: pathlib.Path = None,
-                       expand_cuis=False, apikey=None):
+                       expand_cuis=False, apikey=None, skip_greedy_algorithm=False):
     """
     Run greedy AFEP algorithm on extracted knowledge base articles
 
@@ -53,59 +46,11 @@ def run_afep_algorithm(note_directories, *, mml_format='json', outdir: pathlib.P
     else:
         outdir = pathlib.Path('.')
 
-    article_types = set()
-
-    results = []
-    for path in note_directories:
-        for file in path.glob(f'*.{mml_format}'):
-            article_type = file.stem.split('_')[0]
-            article_types.add(article_type)
-            filename = file.stem
-
-            with open(file) as fh:
-                if mml_format == 'json':
-                    data = json.load(fh)
-                    extract_function = extract_mml_from_json_data
-                elif mml_format == 'mmi':
-                    extract_function = extract_mml_from_mmi_data
-                    data = fh.read()
-                else:
-                    raise ValueError(f'Unrecognized file format for MetaMapLite data: {mml_format}.')
-            extras = {
-                'article_source': article_type,
-            }
-            for result in extract_function(data, filename, extras=extras):
-                results.append(result)
-
+    article_types, results = extract_articles(note_directories, mml_format)
     logger.info(f'Article types: {article_types}')
-    if expand_cuis and UMLS_API_TOOL_INSTALLED:
-        logger.info(f'Trying to expand CUIs...')
-        auth = FriendlyAuthenticator.from_apikey(apikey)
-        known_cuis = {}  # cui -> new terms
-        new_results = []
-        for row in results:
-            target_cui = row['cui']
-            if target_cui not in known_cuis:
-                target_name = row['preferredname']
-                curr_results = []
-                if re.search(r'\b(?:or|and)\b', target_name, re.I):
-                    for term in re.split(r'\W+', target_name):
-                        if term.lower() in {'and', 'or', ''}:
-                            continue
-                        for cui_data in auth.search(term, language='ENG', limit_pages=1):
-                            if cui_data['name'].lower() == term.lower():
-                                curr_results.append({
-                                    'cui': cui_data['cui'],
-                                    'matchedtext': cui_data['name'],
-                                    'length': len(cui_data['name']),
-                                    'offset': target_name.index(term)
-                                })
-                                break  # go to next word
-                known_cuis[target_cui] = curr_results
-            for res in known_cuis[target_cui]:
-                # update cui, matchedtext, start, and length
-                new_results.append(row | res | {'start': row['start'] + res['offset']})
-        results += new_results
+
+    if expand_cuis:
+        results = add_shorter_match_cuis(results, apikey)
 
     logger.info(f'Building pandas dataset from {len(results)} results.')
     df = pd.DataFrame.from_records(results)
@@ -116,42 +61,12 @@ def run_afep_algorithm(note_directories, *, mml_format='json', outdir: pathlib.P
     cui_df = df[df.cui.isin(cuis_with_three_or_more)]
     logger.info(f'Retained {cui_df.shape[0]} CUIs.')
 
-    # greedy algorithm
-    retain_semtypes = {
-        'aapp', 'aapp',
-        # 'anab',  # not sure why was removed?
-        'antb', 'biof', 'bacs', 'bodm', 'chem', 'chvf', 'chvs', 'clnd', 'cgab',
-        'diap', 'dsyn', 'elii', 'enzy', 'fndg', 'hops', 'horm', 'imft', 'irda', 'inbe', 'inpo', 'inch',
-        'lbtr', 'lbpr', 'medd', 'mobd', 'neop', 'nnon', 'orch', 'patf', 'phsu', 'phpr', 'rcpt', 'sosy',
-        'topp', 'vita'
-    }
-    cui_to_source_count = {r.cui: len(r.all_sources.split(',')) for r in
-                           df[['cui', 'all_sources']].drop_duplicates().itertuples()}
-    # use length or not?
-    master_cui = cui_df.query('>0 or '.join(x for x in retain_semtypes if x in cui_df.columns) + '>0')[
-        ['cui', 'matchedtext', 'docid', 'start', 'length']].groupby(
-        ['docid', 'start', 'matchedtext', 'cui']).count().reset_index()
-    # get unique values of docid, matchedtext, start
-    master_cui_as_col = master_cui.pivot(
-        index=['docid', 'matchedtext', 'start'],
-        columns='cui',
-        values='length'
-    ).fillna(0).reset_index()
-    logger.info(f'Unique CUIs: {master_cui["cui"].nunique()}')
-
-    # run greedy algorithm
-    res = []
-    temp_df = master_cui_as_col.copy()
-    while temp_df.shape[0] > 0:
-        _temp_df = temp_df[[col for col in temp_df.columns if str(col).startswith('C')]].sum()  # only CUI columns
-        max_val = _temp_df.max()  # get most frequent
-        cuis = list(_temp_df[_temp_df == max_val].index)  # all CUIs with most frequent value
-        d = {c: cui_to_source_count[c] for c in cuis}
-        cui = max(d, key=d.get)
-        assert cui in cuis
-        res.append(cui)
-        temp_df = temp_df[temp_df[cui] == 0.0]
-    logger.info(f'Result of greedy algorithm: retained {len(res)} CUIs.')
+    # greedy algorithm to reduce number of CUIs
+    if skip_greedy_algorithm:
+        logger.info(f'Skipping greedy algorithm.')
+        res = cui_df['cui'].unique()
+    else:
+        res = run_greedy_algorithm(cui_df, df)
 
     res_dict_df = cui_df[cui_df['cui'].isin(res)].groupby(['cui', 'preferredname']).agg({
         'conceptstring': lambda x: ','.join(set(x)),
