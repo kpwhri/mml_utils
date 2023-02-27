d3f75773e199bb08e197d613620c66816024c7be
dcronkite <dcronkite@gmail.com>
2023-02-27 11:16:33 -0800

    Bump to 0.2.3

--- doco/conf.py
+++ doco/conf.py
@@ -23,7 +23,7 @@ copyright = f'{date.today().year}, KPWHRI'
 author = 'dcronkite <dcronkite@gmail.com>'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.2'
+release = '0.2.3'
 
 # -- General configuration ---------------------------------------------------
 
