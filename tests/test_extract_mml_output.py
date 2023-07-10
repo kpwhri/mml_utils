ac3dea9eaa414848f6e3a84f915b02271224af9f
dcronkite <dcronkite@gmail.com>
2023-07-10 12:11:30 -0700

    Added test to ensure multiple cui mapping works

--- tests/test_extract_mml_output.py
+++ tests/test_extract_mml_output.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 from mml_utils.parse.target_cuis import TargetCuis
 from mml_utils.scripts.extract_mml_output import extract_data_from_file
 
@@ -59,3 +61,39 @@ def test_extract_data_from_file_target_cuis_mapping(fever_file):
             assert data['cui'] == cui
             cnt += 1
     assert cnt == 18
+
+
+def test_extract_data_from_file_target_cuis_mapping(fever_file):
+    cui = 'C4552740'
+    results = list(extract_data_from_file(fever_file, target_cuis=TargetCuis.fromdict({cui: cui, 'C0424755': cui})))
+    cnt = 0
+    for is_record, data in results:
+        if is_record:
+            assert data['processed'] is True
+            assert data['num_chars'] == 2216
+            assert data['num_letters'] == 1796
+        else:
+            assert data['cui'] == cui
+            cnt += 1
+    assert cnt == 18
+
+
+def test_extract_data_from_file_target_multiple_cuis_mapping(fever_file):
+    target_cuis = TargetCuis()
+    cui = 'C4552740'
+    cui2 = 'C0424755'
+    target_cuis.add(cui, cui)
+    target_cuis.add(cui, cui2)
+    target_cuis.add(cui2, cui)
+    results = list(extract_data_from_file(fever_file, target_cuis=target_cuis))
+    cnt = Counter()
+    for is_record, data in results:
+        if is_record:
+            assert data['processed'] is True
+            assert data['num_chars'] == 2216
+            assert data['num_letters'] == 1796
+        else:
+            assert data['cui'] in {cui, cui2}
+            cnt[data['cui']] += 1
+    assert cnt[cui] == 18
+    assert cnt[cui2] == 9
