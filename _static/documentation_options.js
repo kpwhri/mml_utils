abb8d0a77af6030dd1787ac96657d47db7206224
GitHub Action <action@github.com>
2023-09-28 18:14:40 +0000

    Add changes for 72d7101f47c0743c36ffb94083a0be3aee465600

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,13 +1,12 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.3',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.3.5',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
-    SOURCELINK_SUFFIX: '',
+    SOURCELINK_SUFFIX: '.txt',
     NAVIGATION_WITH_KEYS: true,
     SHOW_SEARCH_SUMMARY: true,
     ENABLE_SEARCH_SHORTCUTS: true,
