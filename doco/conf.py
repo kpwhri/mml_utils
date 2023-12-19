074666bdd0cf7b435e928bec00fc92f53eea72e1
dcronkite <dcronkite@gmail.com>
2023-12-19 14:36:20 -0800

    Set version to 0.4.1

--- doco/conf.py
+++ doco/conf.py
@@ -23,7 +23,7 @@ copyright = f'{date.today().year}, KPWHRI'
 author = 'dcronkite <dcronkite@gmail.com>'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.5'
+release = '0.4.1'
 
 # -- General configuration ---------------------------------------------------
 
